a64e22990e0bd135c6f52f366658892dd98ab6a8
Soo <jstensix@naver.com>
2021-04-03 11:50:37 +0900

    init

--- app/src/main/java/com/skt/welfare/Constants.kt
+++ app/src/main/java/com/skt/welfare/Constants.kt
@@ -3,7 +3,7 @@ package com.skt.welfare
 object Constants {
     val baseUrl = "https://m.naver.com"
     val mainCloseText = "'뒤로' 버튼을 한번 더 누르시면 앱이 종료됩니다."
-    val javascriptBridgeName = "bridge"
+    val javascriptBridgeName = "welfare"
     val clipToastText = "클립보드에 복사했습니다"
 
 }
