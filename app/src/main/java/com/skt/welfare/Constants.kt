8407a9a906a583180892fb7ad3858c7690552574
Soo <jstensix@naver.com>
2021-04-26 19:01:00 +0900

    웹뷰 도메인변경

--- app/src/main/java/com/skt/welfare/Constants.kt
+++ app/src/main/java/com/skt/welfare/Constants.kt
@@ -1,8 +1,7 @@
 package com.skt.welfare
 
 object Constants {
-//    val baseUrl = "https://brave-smoke-0254a1600.azurestaticapps.net/#/home"
-    val baseUrl = "https://deploy-front-app.azurewebsites.net/#/home"
+    val baseUrl = "https://stblob2mobile.z32.web.core.windows.net"
     val appCloseWaitText = "'뒤로' 버튼을 한번 더 누르시면 앱이 종료됩니다."
     val appCloseText = "앱이 종료됩니다."
     val javascriptBridgeName = "welfare"
