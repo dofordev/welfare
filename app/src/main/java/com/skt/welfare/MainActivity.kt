b646cfc8183f618529173be16ec35cf321ac8aa7
Soo <jstensix@naver.com>
2021-07-02 21:21:40 +0900

    베네피아 실행 브릿지 추가

--- app/src/main/java/com/skt/welfare/MainActivity.kt
+++ app/src/main/java/com/skt/welfare/MainActivity.kt
@@ -220,7 +220,7 @@ class MainActivity : AppCompatActivity() {
         else{
             mWebView.run {
                 webViewClient = CustomWebViewClient()
-                loadUrl(url)
+                loadUrl(url + "?t=" + System.currentTimeMillis())
             }
         }
 
